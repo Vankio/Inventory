8bb5c932839ccd16cb83ded08a2826d81bc130f3
Vankio <154277622+Vankio@users.noreply.github.com>
2026-08-06 21:58:20 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 # Генерируем безопасный секретный ключ
 SECRET_KEY = os.urandom(24).hex()
-YANDEX_DISK_TOKEN = "y0__xC5nvajqveAAhjS0jcg57KDixNDzwiyBStmdoTyKufu3LHmy8W19w"  # Вставьте сюда ваш OAuth-токен
+YANDEX_DISK_TOKEN = ""  # Вставьте сюда ваш OAuth-токен
